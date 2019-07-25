5f714502484ed8fb49fe7fea832ac0c04043431c
Anthony Baldwin <anthony.baldwin@gmail.com>
2019-07-25 00:57:54 -0700

    wip

--- js/ticker.js
+++ js/ticker.js
@@ -2,71 +2,63 @@
   @TODO: Make this a class already... :|
 */
 
-function addElement (responseText, animations, divId) {
-
+function addElements (responseText, animations, divId) {
+  console.log("Creating elements");
   let speed = animations.initial.speed;
   let animation = animations.initial.type;
+
+  let container = document.createElement("div");
+  container.classList.add('container');
+  document.body.appendChild(container);
 
   let div = document.createElement("div");
   div.textContent = responseText;
   div.setAttribute('id', divId);
-  div.classList.add('animated', animation, speed);
-  document.body.appendChild(div);
+  div.classList.add('content','animated', animation, speed);
+  container.appendChild(div);
 }
 
 function loadTextFile (textFile, callback, directory = "StreamLabels/") {
+  console.log("Loading text file");
   let xmlhttp;
-  if (window.XMLHttpRequest)
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function()
   {
-     xmlhttp = new XMLHttpRequest();
-  }
-  else
-  {
-     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-   }
-   xmlhttp.onreadystatechange = function()
-   {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
-      {
-          if ( typeof callback === 'function' )
-          {
-            callback(xmlhttp.responseText);
-          }
-      }
-  }
+     if (xmlhttp.readyState==4 && xmlhttp.status==200)
+     {
+         if ( typeof callback === 'function' )
+         {
+           callback(xmlhttp.responseText);
+         }
+     }
+ }
   textFile = textFile.replace(/\.[^/.]+$/, "");
   xmlhttp.open("GET", directory+textFile+".txt", true);
-  xmlhttp.send();
+  xmlhttp.send(null);
 }
 
-function loadConfig (tickerLocation, callback, file = "/config/animation.json") {
+function loadAnimationConfig (tickerLocation, callback, file = "/config/animation.json") {
+  console.log("Loading animation config")
   let xmlhttp;
-  if (window.XMLHttpRequest)
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function()
   {
-     xmlhttp = new XMLHttpRequest();
-  }
-  else
-  {
-     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-   }
-   xmlhttp.onreadystatechange = function()
-   {
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
-      {
-          if ( typeof callback === 'function' )
-          {
-            let animations = JSON.parse(xmlhttp.responseText);
-            animations = animations[tickerLocation];
-            callback(animations);
-          }
-      }
+     if (xmlhttp.readyState==4 && xmlhttp.status==200)
+     {
+         if ( typeof callback === 'function' )
+         {
+           let animations = JSON.parse(xmlhttp.responseText);
+           animations = animations[tickerLocation];
+           callback(animations);
+         }
+     }
   }
   xmlhttp.open("GET", file, true);
   xmlhttp.send(null);
 }
 
-function ticker (textFile, tickerLocation, divId = 'content') {
-  loadConfig(tickerLocation, function(responseJson)
+function ticker (textFile, tickerLocation, divId = tickerLocation+'Content') {
+  loadAnimationConfig(tickerLocation, function(responseJson)
   {
     let animations = responseJson;
     let element = document.getElementById(divId);
@@ -76,7 +68,7 @@ function ticker (textFile, tickerLocation, divId = 'content') {
     {
       loadTextFile(textFile, function(responseText)
       {
-        addElement(responseText, animations, divId);
+        addElements(responseText, animations, divId);
       });
     }
     else
@@ -84,15 +76,16 @@ function ticker (textFile, tickerLocation, divId = 'content') {
       loadTextFile(textFile, function(responseText)
       {
         let mainText = document.getElementById(divId);
-        mainText.className = '';
+        mainText.classList = 'content';
 
-        let pageText = mainText.innerHTML;
+        let pageText = mainText.innerHTML.trim();
         pageText = pageText.trim();
-        let newPageText = responseText;
+        let newPageText = responseText.trim();
         newPageText = newPageText.trim();
 
         if (pageText != newPageText)
         {
+          console.log("File: '" + textFile + "' has changed")
           let a = animations;
           mainText.classList.add('animated', a.destroy.type, a.destroy.speed);
           mainText.addEventListener('animationend', function()
